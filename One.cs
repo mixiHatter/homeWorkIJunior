42292766409e4cb9de0dd1662a7927ce0889e5dd
mixiHatter <5281cmixi@gmail.com>
2022-11-29 14:44:05 +0300

    hW is ready
    basis of hW is ready.

---
+++ One.cs
@@ -0,0 +1,20 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Попрактикуйтесь в создании переменных, объявить 10 переменных разных типов.
+        //Напоминание: переменные именуются с маленькой буквы, если название состоит из нескольких слов, то комбинируем их следующим образом - названиеПеременной. lowerCamelCase
+        //Также имя всегда должно отражать суть того, что хранит переменная.
+        int age;
+        float money;
+        bool answer;
+        string message;
+        byte block;
+        short seaLevel;
+        uint iceCreamInStock;
+        double weight;
+        char cipher;
+        long numberOfSeatsInTheAuditorium;
+        var text = "Thx 4 reading";
+    }
+}
