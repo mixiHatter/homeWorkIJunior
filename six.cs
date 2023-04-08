781499152530b7e2008f7ad573b0222d33fb2fa3
mixiHatter <5281cmixi@gmail.com>
2023-04-08 21:11:04 +0300

    Create and done 6 homework
    There were some difficulties with the operators.

---
+++ six.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forTestHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystalPrice = 5;
+            int crystalCount;
+            int remaindGold;
+
+
+            Console.WriteLine("Добро пожаловать в магазин! Сколько у вас золота?");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Кристалл стоит {crystalPrice} голды, какое количество ты хочешь купить?");
+            crystalCount = Convert.ToInt32(Console.ReadLine());
+            remaindGold = gold - crystalCount * crystalPrice;
+            Console.WriteLine($"Теперь у тебя {remaindGold} золото и {crystalCount} кристалла.");
+
+        }
+    }
+}
