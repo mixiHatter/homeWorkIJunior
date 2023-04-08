b86824014797d7ec6509b6365c9ed72fd0386007
mixiHatter <5281cmixi@gmail.com>
2023-04-08 22:05:16 +0300

    Create ten.cs
    and done

---
+++ ten.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forTestHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i <= 96; i+= 7)
+            {
+                Console.WriteLine(i);
+            }
+        }   
+            
+    }
+}
